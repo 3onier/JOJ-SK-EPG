788b20c19db47b5dabfb761b8e3d82e17f39ec21
3onier <30025379+3onier@users.noreply.github.com>
2024-01-21 01:00:50 +0100

    Added Channels

--- src/config/defaultChannels.py
+++ src/config/defaultChannels.py
@@ -4,3 +4,7 @@ PROGRAMM_URL = "https://www.joj.sk/tv-program"
 
 JOJ = Channel("Joj.sk", "https://live.joj.sk/")
 JOJ_PLUS = Channel("Joj-plus.sk", "https://plus.joj.sk/live")
+JOJ_WAU = Channel("Joj-wau.sk", "https://wau.joj.sk/live")
+JOJ_SPORT = Channel("Joj-sport.sk", "https:jojsport.joj.sk/")
+JOJKO = Channel("Jojko.sk", "https://jojko.joj.sk/")
+JOJ_CINEMA = Channel("Joj-cinama.sk", "http://jojcinema.cz/")
